609be6435464237ea7408dec164f2b855d0494f6
davididev <davidwaggener@gmail.com>
2024-02-06 22:38:57 -0500

    Set Vertex selector starting point
    Including UI image and note to self for tomorrow

---
+++ Assets/Mine/Scripts/Components/VertexSelector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexSelector : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public StartingShapeHolder startingShape;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //spriteRenderer.sprite.bounds.Contains()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
